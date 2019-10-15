4fd85b2c60610aaa88fc0e73f9b768585c8c1b91
Mariia Burda <mariiaburda93@gmail.com>
2019-10-15 23:40:22 +0300

    Clean up code

--- homework1_task2_v1.py
+++ homework1_task2_v1.py
@@ -1,17 +1,16 @@
-string = '((((((((((((((2, 3)))))))))))))'
+# check if parenthesis are balanced
 
 
-def balance_parenthesis(stri):
-    sub1 = '('
-    sub2 = ')'
-    count1 = string.count(sub1)
-    count2 = string.count(sub2)
-    if count1 == count2:
+def check_if_parenthesis_are_balanced(input_str):
+    open_sign = '('
+    close_sign = ')'
+    count_open_signs = input_str.count(open_sign)
+    count_close_signs = input_str.count(close_sign)
+    if count_open_signs == count_close_signs:
         print('Parenthesis are balanced')
     else:
         print('Parenthesis are not balanced')
 
 
-balance_parenthesis(string)
-
-
+input_string = '((((((((((((((2, 3)))))))))))))'
+check_if_parenthesis_are_balanced(input_string)
