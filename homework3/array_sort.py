8bcf8267fd0adb1df7976fa350b3b2240d315165
Mariia Burda <mariiaburda93@gmail.com>
2019-11-28 19:34:44 +0200

    homework_3 (array_sort task and number_system_convertor task)

---
+++ homework3/array_sort.py
@@ -0,0 +1,45 @@
+# function for array sorting (bubble sort method)
+
+
+def bubble_sort(array):
+    # go through all elements
+    for i in range(len(array) - 1):
+        elements_are_swapped = False
+        # elements are not swapped yet in this iteration
+        for j in range(0, len(array)-i-1):
+            # go through elements from index 0 to len(array)-i-1
+            # if found element is larger than next, swap them
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                elements_are_swapped = True
+        # if there are no swapped pairs in the iteration, then the array is sorted
+        if not elements_are_swapped:
+            break
+    print('Sorted list: ', array)
+    return array
+
+
+def reversed_array(array):
+    for i in range(len(array) - 1):
+        elements_are_exchanged = False
+        for j in range(0, len(array) - i - 1):
+            # go through elements from index 0 to len(array)-i-1
+            # if found element is smaller than next, swap them
+            if array[j] < array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                elements_are_exchanged = True
+        if not elements_are_exchanged:
+            break
+    print('Reversed_list: ', array)
+
+
+sorted_list = bubble_sort([8, 2, 1, 7, -1, 0.5])
+reversed_array(sorted_list)
+
+
+
+
+
+
+
+
---
+++ homework3/number_system_convertor.py
@@ -0,0 +1,79 @@
+# module for converting a number from any number system to any (up to 36)
+# we use function convert_n_to_m (x, n, m)
+# function convert number x from n-number system to m-number system
+
+import re
+
+
+def val(c):
+    if c >= '0' and c <= '9':
+        return ord(c) - ord('0')
+    else:
+        return ord(c) - ord('A') + 10
+
+
+def toDeci(input_str, from_base):
+    llen = len(input_str)
+    power = 1
+    input_num_in_decimal = 0
+
+    for i in range(llen - 1, -1, -1):
+        if val(input_str[i]) >= from_base:
+            return False
+        input_num_in_decimal += val(input_str[i]) * power
+        power *= from_base
+    if input_num_in_decimal < 0:
+        return False
+    return input_num_in_decimal
+
+
+def reVal(num):
+    if num >= 0 and num <= 9:
+        return chr(num + ord('0'))
+    else:
+        return chr(num - 10 + ord('A'))
+
+
+def fromDeci(to_base, input_num_in_decimal):
+    res = ''
+    if input_num_in_decimal == False:
+        return False
+    else:
+        while input_num_in_decimal > 0:
+            res += reVal(input_num_in_decimal % to_base)
+            input_num_in_decimal = int(input_num_in_decimal / to_base)
+
+        res = res[::-1]
+        return res
+
+
+def check_x_instance(x):
+    try:
+        x = str(x)
+        result = re.findall(r'^\w+$', x)
+        return result[0]
+    except:
+        return 'False'
+
+
+def convert_n_to_m(x, n, m):
+    checking_res = check_x_instance(x)
+    if checking_res == 'False':
+        print(checking_res)
+    elif 1 <= n and m == 1:
+        res = '0' * toDeci(str(x), n)
+        print(res)
+    elif 1 <= n and m <= 36:
+        res = fromDeci(m, toDeci((str(x)), n))
+        print(res)
+    else:
+        print('False')
+
+
+convert_n_to_m([123], 4, 3)
+convert_n_to_m('0123', 5, 6)
+convert_n_to_m('123', 3, 5)
+convert_n_to_m(123, 4, 1)
+convert_n_to_m(-123.0, 11, 16)
+convert_n_to_m('A1Z', 36, 16)
+convert_n_to_m('-123', 4, 3)
